232fbc8f70175fa95c18699f7e4de339379bd874
bountyrushmidman <130882306+bountyrushmidman@users.noreply.github.com>
2023-05-10 00:55:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,9 +38,9 @@ document.getElementById("information").style.display = "block";
 document.getElementById("memName").innerHTML = members[parameter].name;
 document.getElementById("description").innerHTML = members[parameter].description;
 document.getElementById("memName1").innerHTML = members[parameter].name;
-document.getElementById("contactThem").setAttribute("onClick", function() {
-  window.open(members[parameter].contact, "_blank");
-});
+document.getElementById("contactThem").setAttribute("onClick", "window.open(" + members[parameter].contact + ", '_blank')";
+}
+                                                   );
 
 }
 
